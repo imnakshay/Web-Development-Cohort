f598d7d19348fa8bef10cab8d950128ef91b38dc
imnakshay <raviraj7417@gmail.com>
2025-04-04 23:22:31 +0530

    created a backend for user Authentication

---
+++ Week3/SignIn/backend/backend.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+let users = {};
+
+app.use(express.json());
+
+app.post("/signUp",(req,res)=>{
+    const {username,password}=req.body;
+    if(!username || !password){
+        return res.status(400).json({message:"Username or Password can not be empty"});
+    }
+    if(users[username]){
+        return res.status(401).json({message:"Username already exists."});
+    }
+    users[username]=password;
+    return res.json({message:"SignUP successfull"});
+
+})
+
+app.post("/signIn",(req,res)=>{
+    const {username,password}=req.body; 
+    if(!username || !password){
+        return res.status(400).json({message:"Username or Password cannot be empty"});
+    }
+    if(!users[username]){
+        return res.status(401).json({message:"Username not found"});
+    }
+    if(users[username]===password){
+        return res.json({message:"LogIN successfull"})
+    }
+    return res.status(403).json({message:"Login Failed: Wrong username or password"});
+})
+
+app.listen(3000,()=>{
+    console.log("Server Started Please visit : localhost:3000");
+})
